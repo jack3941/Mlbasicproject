6f7e16effe4328cfb0b1eee2c807e972263c35ec
jaiki sagar <jaikisagar@192.168.1.12>
2025-01-27 19:16:25 +0530

    logging and execution

---
+++ app.py
@@ -0,0 +1,16 @@
+from src.MLproject.logger import logging 
+from src.MLproject.exception import CustomException
+import sys 
+
+
+
+
+if __name__ =="__main__":
+    logging.info("execution started")
+
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Custom Exception")
+        raise CustomException(e,sys)
+    
